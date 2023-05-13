cfb998d4319924fdc6b8213755222218dee6dde5
Takina4445 <131225876+Takina4445@users.noreply.github.com>
2023-05-13 16:17:30 +0800

    add food item page
    add the page of item and food

---
+++ app/src/main/java/com/example/dungeon/food.kt
@@ -0,0 +1,15 @@
+package com.example.dungeon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class food : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food)
+
+
+    }//建立時
+
+
+}
