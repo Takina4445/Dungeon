a8a115dd5e0974669c6513633c9a752641dc3061
Takina4445 <131225876+Takina4445@users.noreply.github.com>
2023-05-22 21:07:24 +0800

    add the activitychange
    add the activitychange for mainactivity item food

--- app/src/main/java/com/example/dungeon/MainActivity.kt
+++ app/src/main/java/com/example/dungeon/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.dungeon
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var button_main: Button
+    private lateinit var button_item: Button
+    private lateinit var button_food: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button_main:Button=findViewById<Button>(R.id.button1)
+        val button_item:Button=findViewById<Button>(R.id.button2)
+        val button_food:Button=findViewById<Button>(R.id.button3)
+        button_main.setOnClickListener(ActivityChange_main)
+        button_main.setOnClickListener(ActivityChange_item)
+        button_main.setOnClickListener(ActivityChange_food)
+    }
+    private val ActivityChange_main= View.OnClickListener{
+        //切換至主頁面(戰鬥頁面)
+        val intent = Intent()
+        intent.setClass(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+    private val ActivityChange_item= View.OnClickListener{
+        //切換至主頁面(戰鬥頁面)
+        val intent = Intent()
+        intent.setClass(this, item::class.java)
+        startActivity(intent)
+    }
+    private val ActivityChange_food= View.OnClickListener{
+        //切換至主頁面(戰鬥頁面)
+        val intent = Intent()
+        intent.setClass(this, food::class.java)
+        startActivity(intent)
     }
 }
