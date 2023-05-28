efa3dbd2fb4ace37c3ab7e82d693aa1f04400ba8
Takina4445 <131225876+Takina4445@users.noreply.github.com>
2023-05-28 22:55:06 +0800

    0.6.2

--- app/src/main/java/com/example/dungeon/MyDBHelper.kt
+++ app/src/main/java/com/example/dungeon/MyDBHelper.kt
@@ -11,12 +11,15 @@ class MyDBHelper(context: Context?, name: String?, factory: SQLiteDatabase.Curso
 
     companion object {
         const val DB_TABLE = "player"
+        const val DB_TABLE_MONSTER = "monster"
+        const val DB_TABLE_ITEM="item"
     }
 
     // 建立應用程式需要的表格
     override fun onCreate(db: SQLiteDatabase) {
         createPlayerTable(db)
         insertPlayerData(db)
+        createMonsterTable(db)
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVer: Int, newVer: Int) {
@@ -43,18 +46,34 @@ class MyDBHelper(context: Context?, name: String?, factory: SQLiteDatabase.Curso
         //get(5):"atk"
         //get(6):"def"
         //get(7):"money"
-
-
-
     }
+    private fun createMonsterTable(db: SQLiteDatabase) {
+        val createTableCommand = "CREATE TABLE IF NOT EXISTS $DB_TABLE (" +
+                "id INTEGER PRIMARY KEY," +
+                "name TEXT NOT NULL," +
+                "exp INT," +
+                "hp INT," +
+                "stamina INT," +
+                "atk INT," +
+                "def INT," +
+                "money INT)"
 
+        db.execSQL(createTableCommand)
+        //get(0):id
+        //get(1):"name"
+        //get(2):"exp" 掉落經驗
+        //get(3):"hp" 基礎血量
+        //get(4):"stamina" 基礎體力
+        //get(5):"atk" 基礎攻擊力
+        //get(6):"def" 基礎防禦力
+        //get(7):"money" 掉落金錢
+    }
     private fun insertPlayerData(db: SQLiteDatabase) {
         val playerDataExistQuery = "SELECT COUNT(*) FROM $DB_TABLE"
         val cursor = db.rawQuery(playerDataExistQuery, null)
         cursor.moveToFirst()
         val count = cursor.getInt(0)
         cursor.close()
-
         if (count == 0) {
             val newRow = ContentValues()
             newRow.put("name", "無名的旅人")
@@ -70,5 +89,9 @@ class MyDBHelper(context: Context?, name: String?, factory: SQLiteDatabase.Curso
     fun getPlayerData(): Cursor {
         val db = readableDatabase
         return db.query(DB_TABLE, null, null, null, null, null, null)
+    }
+    fun getMonsterData(): Cursor {
+        val db = readableDatabase
+        return db.query(DB_TABLE_MONSTER, null, null, null, null, null, null)
     }
 }
