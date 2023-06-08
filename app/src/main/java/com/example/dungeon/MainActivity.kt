a4c8645c732b581abb9ff16401d3af678a9caa9d
Takina4445 <131225876+Takina4445@users.noreply.github.com>
2023-06-08 21:59:41 +0800

    ver 0.9.0
    新增部分戰鬥過程描述 新增ContentValues暫時儲存玩家與怪物數據

--- app/src/main/java/com/example/dungeon/MainActivity.kt
+++ app/src/main/java/com/example/dungeon/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.dungeon
 
+import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
@@ -10,12 +11,14 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Html
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import com.example.dungeon.MyDBHelper.Companion.DB_TABLE_MONSTER
 import org.w3c.dom.Text
 import java.util.Arrays
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var button_main: Button
@@ -30,7 +33,7 @@ class MainActivity : AppCompatActivity() {
     val DB_TABLE_SKILLS="skills"
     val DB_TABLE_USE_SKILL="useskill"
     val DB_TABLE_ITEM="item"
-
+var i:Int=0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,7 +50,8 @@ class MainActivity : AppCompatActivity() {
         button_food.setOnClickListener(ActivityChange_food)
         button_battle.setOnClickListener(Activity_battle)
         player_status_change()//刷新玩家資料
-
+        val battle_log:TextView=findViewById<TextView>(R.id.textview_battleprocess)//戰鬥紀錄
+        battle_log.text= ""
 
     }
 
@@ -90,9 +94,11 @@ class MainActivity : AppCompatActivity() {
         intent.setClass(this, Food::class.java)
         startActivity(intent)
     }
+
     private val Activity_battle=View.OnClickListener {
         val battle_log:TextView=findViewById<TextView>(R.id.textview_battleprocess)//戰鬥紀錄
-        battle_log.text=""
+//        battle_log.text= ""
+
         val MyDB: SQLiteDatabase
 // 建立自訂的 FriendDbHelper 物件
         val friDbHp = MyDBHelper(applicationContext, DB_FILE, null, 1)
@@ -100,9 +106,102 @@ class MainActivity : AppCompatActivity() {
         MyDB = friDbHp.writableDatabase
         val c = friDbHp.getPlayerData()
         c.moveToFirst()
+        val obj_player = ContentValues()
+//        將玩家數值存入
+        var columnCount = c.columnCount-1
+        do{
+            for (i in 0..columnCount) {
+                val columnName = c.getColumnName(i)
+                val columnType = c.getType(i)
+                when (columnType) {
+                    Cursor.FIELD_TYPE_INTEGER -> {
+                        val intValue = c.getInt(i)
+                        obj_player.put(columnName, intValue)
+                    }
+
+                    Cursor.FIELD_TYPE_STRING -> {
+                        val stringValue = c.getString(i)
+                        obj_player.put(columnName, stringValue)
+                    }
+                }
+            }
+        }while (c.moveToNext())
+        c.moveToFirst()
+//怪物選擇
+        val selection = "id = ?"
+        val selectionArgs = arrayOf(Random.nextInt(1, 3).toString())
+        val cursor = MyDB.query(DB_TABLE_MONSTER,null ,selection, selectionArgs, null, null, null)
+//        val columnName = "*"
+//        arrayOf(columnName)
+
+        cursor.moveToFirst()
+
+        val obj_monster = ContentValues()
+//        將怪物數值存入
+        columnCount = cursor.columnCount-1
+        do{
+            for (i in 0..columnCount) {
+                val columnName = cursor.getColumnName(i)
+                val columnType = cursor.getType(i)
+                when (columnType) {
+                    Cursor.FIELD_TYPE_INTEGER -> {
+                        val intValue = cursor.getInt(i)
+                        obj_monster.put(columnName, intValue)
+                    }
+
+                    Cursor.FIELD_TYPE_STRING -> {
+                        val stringValue = cursor.getString(i)
+                        obj_monster.put(columnName, stringValue)
+                    }
+                }
+            }
+        }while (cursor.moveToNext())
+        cursor.moveToFirst()
 
 
 
+//        val monsterName =obj_monster.get("name")
+//        battle_log.text=monsterName
+        var var_totalweight:Int
+        var var_battle_random:Int
+
+
+//        while (cursor.getInt(5)>0||c.getInt(4)>0){
+            var var_battle_damage:Int//戰鬥傷害
+            var_totalweight=obj_player.getAsInteger("stamina")+obj_monster.getAsInteger("stamina")
+
+
+            var_battle_random= Random.nextInt(0,var_totalweight+2)
+//        if (var_battle_random <= c.getInt(6) * (c.getInt(11) / 100)) {
+            if (var_battle_random <= obj_player.getAsInteger("stamina") * (obj_player.getAsInteger("speed")/ obj_monster.getAsInteger("speed"))*0.9) {
+                //玩家回合
+                var_battle_damage=(obj_player.getAsInteger("atk")*1.3-obj_monster.getAsInteger("def")).toInt()
+                battle_log.append(Html.fromHtml(
+                        "<font color=${Color.CYAN}>" + obj_player.get("name") + "&nbsp;</font>"
+                                + "<font color=${Color.WHITE}>對&nbsp;</font>"
+                                + "<font color=${Color.RED}>" + obj_monster.get("name") + "&nbsp;</font>"
+                                + "<font color=${Color.WHITE}>使出&nbsp;</font>" + "<font color=${Color.YELLOW}>" + "攻擊" + "&nbsp;</font>"
+                                + "<font color=${Color.WHITE}>造成</font>" + "<font color=${Color.WHITE}>" + var_battle_damage + "點傷害<br></font>",
+                        Html.FROM_HTML_MODE_LEGACY))
+
+            }
+
+            else {
+                //怪物回合
+                battle_log.append(Html.fromHtml(
+                    "<font color=${Color.RED}>" + obj_monster.get("name") + "&nbsp;</font>"
+                            + "<font color=${Color.WHITE}>對&nbsp;</font>"
+                            + "<font color=${Color.CYAN}>" + obj_player.get("name") + "&nbsp;</font>"
+                            + "<font color=${Color.WHITE}>使出&nbsp;</font>" + "<font color=${Color.YELLOW}>" + "攻擊" + "&nbsp;</font>"
+                            + "<font color=${Color.WHITE}>造成</font>" + "<font color=${Color.WHITE}>" + 3 + "點傷害<br></font>",
+                    Html.FROM_HTML_MODE_LEGACY))
+            }
+
+            cursor.close()
+//        }
+
+
+/*
         val newRow = ContentValues()
         val monsterName = "野豬"
         val columnName = "exp"
@@ -120,6 +219,9 @@ class MainActivity : AppCompatActivity() {
             "id='1'", null)
         player_status_change()
         cursor.close()
+        */
+
+
     }
 
     /////////
